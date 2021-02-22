67f39628674fc9919fc646a02b304f3068150b13
Kunal Choudhary <kunal13053@iiitd.ac.in>
2021-02-23 05:14:44 +0530

    Appbar added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 // import styled, {css} from 'styled-components'
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <AppLayout>
+          <AppBar />
           <WelcomeMessage />
         </AppLayout>
       </header>
--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import styled, {css} from 'styled-components'
 
-export default function WelcomeMessage() {
+export default function () {
     return (
         <div>
             <h1>Welcome to CryptoDash!</h1>
