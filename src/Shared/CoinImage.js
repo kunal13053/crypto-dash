c48fd9cf7665fee2177169e9760c1f1ea97751b6
Kunal Choudhary <kunal13053@iiitd.ac.in>
2021-02-28 18:52:26 +0530

    Data added to coin tiles

---
+++ src/Shared/CoinImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ({coin, style}) {
+    return <img 
+        alt={coin.coinSymbol}
+        style={style || {height: '50px'}}
+        src={`http://cryptocompare.com/${coin.ImageUrl}`}
+    />
+}
